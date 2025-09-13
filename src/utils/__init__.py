632c95b754ae08cbd3528d256e1bb66fcc8b2ccb
raichi1 <renzoec.u@gmail.com>
2025-09-13 12:15:37 -0500

    feat: add dataset UCI-HAR to test model generalized

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
-from .data_loading import load_sensors_separately
+from .data_loading import load_sensors_separately, normalize_columns, convert_timestamp
+from .assign_activity import assign_activity
 
-__all__ = ['load_sensors_separately']
+__all__ = ['load_sensors_separately', 'normalize_columns', 'convert_timestamp', 'assign_activity']
